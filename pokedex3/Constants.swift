2971d8280fb939d89e67d33df3d916d76282a87a
beeerod <beeerod@live.com>
2017-09-06 23:29:23 -0700

    First Commit

---
+++ pokedex3/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  pokedex3
+//
+//  Created by BRANDON RODRIGUEZ on 9/5/17.
+//  Copyright © 2017 BRodz. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "https://pokeapi.co"
+
+let URL_POKEMON = "/api/v1/pokemon/"
+
+typealias DownloadComplete = () -> ()
